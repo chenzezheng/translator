bd47d20c1b7fc41466198f91e296e191f5e5141e
Akakii <akakiichan@foxmail.com>
2020-06-19 02:36:06 +0800

    添加了截图功能

---
+++ client/TranslatorUI/RisCaptureLib/MaskWindow.cs
@@ -0,0 +1,162 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace RisCaptureLib
+{
+    internal class MaskWindow : Window
+    {
+        private MaskCanvas innerCanvas;
+        private Bitmap screenSnapshot;
+        private Timer timeOutTimmer;
+        private readonly ScreenCaputre screenCaputreOwner;
+
+        public MaskWindow(ScreenCaputre screenCaputreOwner)
+        {
+            this.screenCaputreOwner = screenCaputreOwner;
+            Ini();
+        }
+
+        private void Ini()
+        {
+            
+            //ini normal properties
+            //Topmost = true;
+            WindowStyle = WindowStyle.None;
+            ResizeMode = ResizeMode.NoResize;
+            ShowInTaskbar = false;
+
+            //set bounds to cover all screens
+            var rect = SystemInformation.VirtualScreen;
+            Left = rect.X;
+            Top = rect.Y;
+            Width = rect.Width;
+            Height = rect.Height;
+
+            //set background 
+            screenSnapshot = HelperMethods.GetScreenSnapshot();
+            if (screenSnapshot != null)
+            {
+                var bmp = screenSnapshot.ToBitmapSource();
+                bmp.Freeze();
+                Background = new ImageBrush(bmp);
+            }
+
+            //ini canvas
+            innerCanvas = new MaskCanvas
+            {
+                MaskWindowOwner = this
+            };
+            Content = innerCanvas;
+
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if(e.RightButton == MouseButtonState.Pressed && e.ClickCount>=2)
+            {
+                CancelCaputre();
+            }
+        }
+
+        protected override void OnMouseMove(System.Windows.Input.MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if(timeOutTimmer != null && timeOutTimmer.Enabled)
+            {
+                timeOutTimmer.Stop();
+                timeOutTimmer.Start();
+            }
+        }
+
+        protected override void OnKeyDown(System.Windows.Input.KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if(e.Key == Key.Escape)
+            {
+                CancelCaputre();
+            }
+        }
+
+        private void CancelCaputre()
+        {
+            Close();
+            screenCaputreOwner.OnScreenCaputreCancelled(null);
+        }
+
+        internal void OnShowMaskFinished(Rect maskRegion)
+        {
+            
+        }
+
+        internal void ClipSnapshot(Rect clipRegion)
+        {
+            BitmapSource caputredBmp = CopyFromScreenSnapshot(clipRegion);
+
+            if (caputredBmp != null)
+            {
+                screenCaputreOwner.OnScreenCaputred(null, caputredBmp);
+            }
+
+            //close mask window
+            Close();
+        }
+
+
+        internal BitmapSource CopyFromScreenSnapshot(Rect region)
+        {
+            var sourceRect = region.ToRectangle();
+            var destRect = new Rectangle(0, 0, sourceRect.Width, sourceRect.Height);
+
+            if (screenSnapshot != null)
+            {
+                var bitmap = new Bitmap(sourceRect.Width, sourceRect.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.DrawImage(screenSnapshot, destRect, sourceRect, GraphicsUnit.Pixel);
+                }
+
+                return bitmap.ToBitmapSource();
+            }
+
+            return null;
+        }
+
+        public void Show(int timeOutSecond, System.Windows.Size? defaultSize)
+        {
+            if (timeOutSecond > 0)
+            {
+                if (timeOutTimmer == null)
+                {
+                    timeOutTimmer = new Timer();
+                    timeOutTimmer.Tick += OnTimeOutTimmerTick;
+                }
+                timeOutTimmer.Interval = timeOutSecond*1000;
+                timeOutTimmer.Start();
+            }
+
+            if(innerCanvas != null)
+            {
+                innerCanvas.DefaultSize = defaultSize;
+            }
+
+            Show();
+            Focus();
+
+        }
+
+        private void OnTimeOutTimmerTick(object sender, System.EventArgs e)
+        {
+            timeOutTimmer.Stop();
+            CancelCaputre();
+        }
+
+        
+    }
+}
--- client/TranslatorUI/TranslatorUI/Pages/TransPage.xaml.cs
+++ client/TranslatorUI/TranslatorUI/Pages/TransPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,6 +21,8 @@ namespace TranslatorUI.Pages
     /// </summary>
     public partial class TransPage : UserControl
     {
+        private readonly RisCaptureLib.ScreenCaputre screenCaputre = new RisCaptureLib.ScreenCaputre();
+        private Size? lastSize;
         List<string> language = new List<string>();
         public TransPage()
         {
@@ -31,6 +34,39 @@ namespace TranslatorUI.Pages
             language.Add("French");
             this.languageBeforeTrans.DataContext = language;
             this.languageAfterTrans.ItemsSource = language;
+
+            screenCaputre.ScreenCaputred += OnScreenCaputred;
+            screenCaputre.ScreenCaputreCancelled += OnScreenCaputreCancelled;
+        }
+
+        private void OnScreenCaputreCancelled(object sender, System.EventArgs e)
+        {
+            //Show(); 这里是显示当前页面
+            Focus();
+        }
+
+        private void OnScreenCaputred(object sender, RisCaptureLib.ScreenCaputredEventArgs e)
+        {
+            //set last size
+            lastSize = new Size(e.Bmp.Width, e.Bmp.Height);
+
+
+            //Show();  这里是显示当前页面
+
+            //test
+            var bmp = e.Bmp;
+            var win = new Window { SizeToContent = SizeToContent.WidthAndHeight, ResizeMode = ResizeMode.NoResize };
+
+            var canvas = new Canvas { Width = bmp.Width, Height = bmp.Height, Background = new ImageBrush(bmp) };
+
+            win.Content = canvas;
+            win.Show();
+        }
+        private void cutScreen_btn_Click(object sender, RoutedEventArgs e)
+        {
+            //Hide();  这里是隐藏当前页面
+            Thread.Sleep(300);
+            screenCaputre.StartCaputre(30, lastSize);
         }
     }
 
